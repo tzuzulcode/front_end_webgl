cc9783dac656ab955dc75b7456b964b3a660a0f3
Tzuzul <mail@tzuzulcode.com>
2022-08-23 22:12:08 -0500

    Key events

--- introduccion/sketch.js
+++ introduccion/sketch.js
@@ -5,15 +5,28 @@ const middleWidth = width/2
 const middleHeight = height/2
 
 
-let randomX1 //variable sin inicializar
-let randomY1
-let trianglePoints
+// let randomX1 //variable sin inicializar
+// let randomY1
+// let trianglePoints
+
+let valueX = middleWidth
+let valueY = middleHeight
+let pacmanSize = 100
+let circleSize = 50
+
+let circles = []
 
 function setup() { // Configuracion del canvas y solo se ejecuta
   createCanvas(width, height);
-  randomX1 = random(width) // Aleatorio entre 0 y 500
-  randomY1 = random(height)
-  trianglePoints = [random(height),random(height),random(height),random(height),random(height),random(height)]
+  // randomX1 = random(width) // Aleatorio entre 0 y 500
+  // randomY1 = random(height)
+  // trianglePoints = [random(height),random(height),random(height),random(height),random(height),random(height)]
+  for(let i=0;i<10;i++){
+    circles.push({
+      x:random(width),
+      y:random(height),
+    })
+  }
 }
 
 function draw() {
@@ -25,10 +38,10 @@ function draw() {
 
   // noFill()
   fill(255)
-  rect(randomX1,randomY1,50,50)
-  triangle(...trianglePoints)
+  // rect(randomX1,randomY1,50,50)
+  // triangle(...trianglePoints)
   // x,y,h,w,start,end
-  arc(50,50,50,50,0,2*PI)
+  // arc(50,50,50,50,0,2*PI)
   
   if(mouseIsPressed){
     fill(240, 58, 71)
@@ -36,11 +49,57 @@ function draw() {
     fill(255)
   }
   
-  ellipse(mouseX,mouseY,50) //Circulo o elipse
+  circles.forEach(circle=>{
+    ellipse(circle.x,circle.y,circleSize) //Circulo o elipse
+  })
 
   // const randomX1 = random(500)
   // const randomY1 = random(500)
 
+  // fill(245, 224, 91)
+  // strokeWeight(7)
+  // stroke(0)
+  // arc(width / 2, height / 2, 100, 100, PI / 4, (7 * PI) / 4, PIE)
 
-  console.log("Draw")
+  if(keyIsPressed){
+    if(keyCode === LEFT_ARROW){
+      valueX=valueX-2
+    }else if(keyCode === RIGHT_ARROW){
+      valueX=valueX+2
+    }else if(keyCode === UP_ARROW){
+      valueY-=2
+    }else if(keyCode === DOWN_ARROW){
+      valueY+=2
+    }
+  }
+
+  verifyIfPacmanIsTouchingCircles()
+
+
+
+  fill(255, 255, 0)
+  arc(valueX,valueY,pacmanSize,pacmanSize,radians(30),radians(330), PIE)
+  
+  fill(0,0,0)
+  ellipse(valueX+10,valueY-30,15,15)
+
+  // console.log("Draw")
+}
+
+// function keyPressed(){
+//   if(keyCode === LEFT_ARROW){
+//     valueX=valueX-1
+//   }
+// }
+
+
+function verifyIfPacmanIsTouchingCircles(){
+  const newCircles = circles.filter(circle=>{
+    let distance = Math.sqrt(Math.pow(valueX-circle.x,2)+Math.pow(valueY-circle.y,2))
+    if(distance>(pacmanSize/2 + circleSize/2)){
+      return circle
+    }
+  })
+
+  circles = newCircles
 }
